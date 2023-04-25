801386c762118842044d9793e3aa56bbca6a8ab6
danijel1023 <>
2023-04-25 13:21:07 +0200

    Add base window

--- include/BaseWindow.h
+++ include/BaseWindow.h
@@ -1,1 +1,39 @@
 #pragma once
+
+#include <string>
+#include "units.h"
+
+namespace Galvanizer
+{
+
+class BaseWindow;
+
+using BWinHNDL = BaseWindow*;
+using WinFactoryPtr = BWinHNDL(*)();
+
+
+class BaseWindow
+{
+public:
+    BaseWindow(BWinHNDL parent, const std::string& name, Vec2 size, Vec2 pos);
+    BaseWindow(const std::string& name, Vec2 size, Vec2 pos);
+    virtual ~BaseWindow();
+
+    std::string getPath();
+
+    inline const std::string& getName() { return p_name; }
+
+public:
+    Vec2 size, pos;
+
+protected:
+    std::string p_name;
+    BWinHNDL p_parent = nullptr;
+    BWinHNDL p_mainWindow = nullptr;
+
+private:
+
+    friend class Application;
+};
+
+}
